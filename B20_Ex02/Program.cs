b771780b7e729eaf834aa066fc128c531adf0b43
Ron Koifman <ronkoifman94@gmail.com>
2020-05-20 17:08:52 +0300

    Add project files.

---
+++ B20_Ex02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace B20_Ex02
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Running!");
+        }
+    }
+}
